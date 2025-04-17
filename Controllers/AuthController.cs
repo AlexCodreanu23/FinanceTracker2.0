1474f6bcd6c42e67e94c548d5cac1a3787c2f7b6
AlexCodreanu23 <AlexCosmin2377@gmail.com>
2025-04-17 21:35:26 +0300

    added Register Controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,39 +3,67 @@ using FinanceTracker_2._0.Models;
 using FinanceTracker_2._0.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace FinanceTracker_2._0.Controllers
 {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class AuthController : ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IJwtTokenService _jwtTokenService;
+
+        public AuthController(UserManager<User> userManager, IJwtTokenService jwtTokenService)
         {
-            private readonly UserManager<User> _userManager;
-            private readonly IJwtTokenService _jwtTokenService;
+            _userManager = userManager;
+            _jwtTokenService = jwtTokenService;
+        }
 
-            public AuthController(UserManager<User> userManager, IJwtTokenService jwtTokenService)
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDTO.Password))
+                return Unauthorized("Invalid credentials.");
+
+            var token = _jwtTokenService.GenerateToken(user);
+            return Ok(new
             {
-                _userManager = userManager;
-                _jwtTokenService = jwtTokenService;
+                token,
+                userId = user.Id,
+                firstName = user.firstName,
+                lastName = user.lastName
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO dto)
+        {
+            var user = new User
+            {
+                UserName = dto.Email,
+                Email = dto.Email,
+                firstName = dto.FirstName,
+                lastName = dto.LastName,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
             }
 
-            [HttpPost("login")]
-            public async Task<ActionResult> Login([FromBody] LoginDTO loginDTO)
+            var token = _jwtTokenService.GenerateToken(user);
+            return Ok(new
             {
-                var user = await _userManager.FindByEmailAsync(loginDTO.Email);
-                if (user == null)
-                {
-                    return Unauthorized("Invalid credentials.");
-                }
-
-                var result = await _userManager.CheckPasswordAsync(user, loginDTO.Password);
-                if (!result)
-                {
-                    return Unauthorized("Invalid credentials.");
-                }
-
-                var token = _jwtTokenService.GenerateToken(user);
-                return Ok(new { token });
-            }
+                token,
+                firstName = user.firstName,
+                lastName = user.lastName
+            });
         }
     }
+}
